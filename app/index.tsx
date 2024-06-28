ff6389f6e869654246e6909765c7e60a9ee1b9e6
Faysal Bsata <faysalbsata@hotmail.com>
2024-06-28 22:22:29 +0400

    statusbar color and header hide

--- app/index.tsx
+++ app/index.tsx
@@ -3,12 +3,14 @@ import Map from '~/components/Map';
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
+import { StatusBar } from 'expo-status-bar';
 
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Home' }} />
+      <Stack.Screen options={{ title: 'Home', headerShown: false }} />
       <Map />
+      <StatusBar style="inverted" />
     </>
   );
 }
